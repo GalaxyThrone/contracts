1c907d38d3bbaa53925e0b7237c7eb190b199604
Ender13120 <ender.mat@hotmail.de>
2022-11-13 15:36:07 +0100

    deployment runs for testing

--- test/test.ts
+++ test/test.ts
@@ -7,34 +7,74 @@ import {
   Buildings,
   Planets,
   Fleets,
-  GameFacet,
   BuildingsFacet,
+  RegisterFacet,
 } from "../typechain-types";
 import { BigNumber } from "ethers";
 import { impersonate } from "../scripts/helperFunctions";
-import { upgrade } from "../scripts/upgradeDiamond";
+import {
+  upgrade,
+  upgradeTestVersion,
+} from "../scripts/upgradeDiamond";
+
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
 // import { upgradeContract } from "../scripts/upgradeContract";
 
-let g: any;
-let gameFacet: GameFacet;
-let buildingsFacet: BuildingsFacet;
-
 describe("Game", function () {
-  before(async function () {
-    this.timeout(20000000);
-    // g = await deployDiamond();
+  let g: any;
 
-    await upgrade();
+  let vrfFacet: RegisterFacet;
+  let buildingsFacet: BuildingsFacet;
+
+  async function deployUsers() {
+    const [
+      owner,
+      randomUser,
+      randomUserTwo,
+      randomUserThree,
+      AdminUser,
+    ] = await ethers.getSigners();
+
+    return {
+      owner,
+      randomUser,
+      randomUserTwo,
+      randomUserThree,
+      AdminUser,
+    };
+  }
+
+  before(async function () {
+    // this.timeout(20000000);
+    g = await deployDiamond();
+
+    const diamond = g.diamondAddress; //"0xe296a5cf8c15d2a4192670fd12132fc7a2d5f426";
+    //await upgrade();
+
+    //@why the fuck is this empty
+    console.log(
+      "-------------------------------------------------------------------"
+    );
+    console.log(g.diamondAddress);
+    console.log(
+      "-------------------------------------------------------------------"
+    );
+
+    await upgradeTestVersion(diamond);
     // await upgradeContract("0xA7902D5fd78e896A1071453D2e24DA41a7fA0004");
 
-    const diamond = "0xe296a5cf8c15d2a4192670fd12132fc7a2d5f426";
+    vrfFacet = (await ethers.getContractAt(
+      "RegisterFacet",
+      diamond
+    )) as RegisterFacet;
 
-    // vrfFacet = (await ethers.getContractAt("VRFFacet", diamond)) as VRFFacet;
-    gameFacet = (await ethers.getContractAt("GameFacet", diamond)) as GameFacet;
     buildingsFacet = (await ethers.getContractAt(
       "BuildingsFacet",
       diamond
     )) as BuildingsFacet;
+
     // actionsFacet = (await ethers.getContractAt(
     //   "ActionsFacet",
     //   diamond
@@ -44,7 +84,23 @@ describe("Game", function () {
     //   "0x43D900698f716179794553B5BCaacb0e37080828"
     // )) as MagiaLands;
   });
-  it("debug", async function () {
+  it("register user and get planet NFT ", async function () {
+    const {
+      owner,
+      randomUser,
+      randomUserTwo,
+      randomUserThree,
+      AdminUser,
+    } = await loadFixture(deployUsers);
+
+    const registration = await vrfFacet
+      .connect(randomUser)
+      .register();
+
+    //@TODO MOCK VRF
+  });
+
+  it.skip("debug", async function () {
     buildingsFacet = await impersonate(
       "0xf2381dD8B282669C139C2d227bAb5314B5E9EBC7",
       buildingsFacet,
